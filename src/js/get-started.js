90c2bb689c1de82c037c4369c6f24b6a7905ef70
mestn1y <mestn1y@hotmail.com>
2025-05-30 23:53:11 +0300

    change get-strated js

--- src/js/get-started.js
+++ src/js/get-started.js
@@ -1,34 +1,36 @@
 import Swiper from 'swiper';
 
-import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper/modules';
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Autoplay,
+  EffectCoverflow,
+} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import 'swiper/css/effect-coverflow';
+
+Swiper.use([Navigation, Pagination, Scrollbar, Autoplay]);
 
 const swiper = new Swiper('.mySwiper', {
+  modules: [Pagination, Autoplay],
   effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: 3,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2,
-    slideShadows: true,
-    scale: 1.3,
-  },
   keyboard: {
     enabled: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
   },
   mousewheel: {
     thresholdDelta: 70,
   },
   spaceBetween: 30,
   loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
 });
