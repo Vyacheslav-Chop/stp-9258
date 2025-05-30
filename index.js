baf560782cd5fdb87d03fffa914b1a7bf47ec7b4
Vyacheslav-Chop <syavchik260119901@gmail.com>
2025-05-30 17:18:05 +0000

    Deploying to gh-pages from @ Vyacheslav-Chop/stp-9258@15bf0c739f323aae60bfd43b77c9e66f73280e0c 🚀

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-/* empty css                      */(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&c(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const l=document.querySelector('[data-action="open"]'),d=document.querySelector('[data-action="close"]'),i=document.querySelector("[data-visible]");l.addEventListener("click",n=>{i.dataset.visible="open"});d.addEventListener("click",n=>{i.dataset.visible="close"});
+/* empty css                      */import{S as a,N as d,P as o,a as p,A as l}from"./assets/vendor-JLaRIylK.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function u(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=u(e);fetch(e.href,t)}})();const m=document.querySelector('[data-action="open"]'),g=document.querySelector('[data-action="close"]'),c=document.querySelector("[data-visible]");m.addEventListener("click",r=>{c.dataset.visible="open"});g.addEventListener("click",r=>{c.dataset.visible="close"});const f=[{review:"So satisfying! The twist mechanics are brilliant. I’ve never played anything like it!",username:"Ava N.",rating:5},{review:"Clean design, relaxing sounds, and deceptively tricky puzzles. 10/10!",username:"Tyler M.",rating:4},{review:"The rotating layers make every level feel fresh and challenging.",username:"Mina R.",rating:4},{review:"Perfect puzzle game. Addictive, calm, and clever. I play it on my commute!",username:" Eric D.",rating:4},{review:"Would love more customization themes but the gameplay is fantastic!",username:"Chloe L.",rating:4}],v=r=>Array.from({length:r},()=>`
+    <li>
+      <svg width="20" height="20">
+        <use href="../img/sprite.svg#icon-star"></use>
+      </svg>
+    </li>
+  `).join("");a.use([d,o,p,l]);const y=document.querySelector(".swiper-wrapper"),w=f.map((r,i)=>`
+      <ul class="swiper-slide">
+        <li class="list-item" data-id="${i}">
+          <p class="review-text">"${r.review}"</p>
+          <div class="reviewer-info">
+            <ul class="reviewer-rating-list">
+              ${v(r.rating)}
+            </ul>
+            <p class="review-name-text">${r.username}</p>
+          </div>
+        </li>
+      </ul>
+    `).join("");y.innerHTML=w;new a(".swiper",{modules:[o,l],slidesPerView:1,spaceBetween:20,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:3e3,disableOnInteraction:!1},breakpoints:{320:{slidesPerView:1},1200:{slidesPerView:3}}});
 //# sourceMappingURL=index.js.map
