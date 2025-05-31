45c40cfec9c64ae61cf834c3e0d47796008f5f5c
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-05-31 23:58:35 +0300

    markup header & modal-menu

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -4,8 +4,18 @@ const burgerMenuEl = document.querySelector('[data-visible]');
 
 openBtnEl.addEventListener('click', e => {
   burgerMenuEl.dataset.visible = 'open';
+  document.body.style.overflow = 'hidden';
 });
 
 closeBtnEl.addEventListener('click', e => {
   burgerMenuEl.dataset.visible = 'close';
+  document.body.style.overflow = '';
+});
+
+burgerMenuEl.addEventListener('click', ev => {
+  const nav = ev.target.closest('[data-nav]');
+  if (nav) {
+    burgerMenuEl.dataset.visible = 'close';
+    document.body.style.overflow = '';
+  }
 });
