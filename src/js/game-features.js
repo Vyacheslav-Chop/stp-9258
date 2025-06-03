42ad019720363ef7e4cdbf6c20f126c6ad319e9d
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-06-03 19:22:21 +0300

    Disabled Swiper for desktops

--- src/js/game-features.js
+++ src/js/game-features.js
@@ -1,32 +1,40 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper/modules';
+import { Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-Swiper.use([Navigation, Pagination, Scrollbar, Autoplay]);
+let featuresSwiper;
 
-new Swiper('.game-features-swiper', {
-  modules: [Pagination, Autoplay],
-  slidesPerView: 1,
-  spaceBetween: 20,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    320: {
+const screenWidthQuery = window.matchMedia('(max-width: 1199.98px)');
+
+function initFeaturesSwiper() {
+  if (screenWidthQuery.matches && !featuresSwiper) {
+    featuresSwiper = new Swiper('.game-features-swiper', {
+      modules: [Pagination, Autoplay],
       slidesPerView: 1,
-      allowTouchMove: true,
-    },
-    1200: {
-      slidesPerView: 4,
-      allowTouchMove: true,
-    },
-  },
-});
+      spaceBetween: 20,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          allowTouchMove: true,
+        },
+      },
+    });
+  } else if (!screenWidthQuery.matches && featuresSwiper) {
+    featuresSwiper.destroy(true, true);
+    featuresSwiper = null;
+  }
+}
+
+initFeaturesSwiper();
+screenWidthQuery.addEventListener('change', initFeaturesSwiper);
